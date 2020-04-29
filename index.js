f6706b87256ed248cc8aa2cbbd221d466e1813e8
Frederick-88 <frederickfd88@gmail.com>
2020-04-29 09:38:51 +0700

    COMMIT2

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const app = require('express')
+const app = require('express')()
 const http = require('http').createServer(app);
+const socketIo = require('socket.io')(http)
 
 app.get('/', (req,res) => {
-    res.send('<h1>Chat App</h1>')
+    // res.send('<h1>Chat App</h1>')
+    res.sendFile(__dirname + '/index.html')
+})
+
+socketIo.on('connection', (socket)=>{
+    console.log('a user connected');
+    
+
 })
 
 http.listen(4000, () => {
